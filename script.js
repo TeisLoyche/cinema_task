3e6e64935a52fa98c5ee4be707c54b52445200f4
Teis Loyche <teisloyche@hotmail.com>
2024-04-23 09:34:23 +0200

    log seats

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*Försöker bara testa med 2 säten till att börja med. Får det dock inte att fungera*/
 
+/* 
 document.addEventListener("DOMContentLoaded", function () {
   const seatData = [
     { id: "seat1-1", name: "seatOne" },
@@ -36,4 +37,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById(("selectedTickets".innerHTML = ""));
   });
-});
+}); */
+
+/*Logga ett seat när man klickar*/
+
+/* 
+const selectedSeats = [];
+
+function myFunction1() {
+  const seat = document.getElementById("seat1-11");
+  selectedSeats.push(seat);
+}
+
+function myFunction2() {
+  const seat = document.getElementById("seat1-10");
+  selectedSeats.push(seat);
+}
+
+function book() {
+  selectedSeats.forEach((seat) => alert(seat));
+}
+*/
+
+/*Lägger alla checkboxar med namnet "checkbox" i variabeln.*/
+let checkbox = document.getElementsByName("checkbox");
+
+//Skapar en tom array där man kan spara de checkboxar som klickas i.
+let seats = [];
+
+//Lägger till ett click event på alla checkboxar, och lägger till varje checkbox man klickar på i den tomma arrayen.
+for (let i = 0; i < checkbox.length; i++) {
+  checkbox[i].addEventListener("click", () => {
+    seats.push(checkbox[i]);
+  });
+}
+
+//Loggar ut samtliga objekt i arrayen.
+function displaySeats() {
+  console.log(seats);
+}
