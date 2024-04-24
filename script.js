85fb92cbfca13903a2f9d9fab4e447354b5aa4c5
Teis Loyche <teisloyche@hotmail.com>
2024-04-24 15:47:50 +0200

    Added some final comments

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ let checkbox = document.getElementsByName("checkbox");
 let seats = [];
 
 //Lägger till ett click event på alla checkboxar, och lägger till varje checkbox man klickar på i den tomma arrayen.
+// Inspiration hämtad härifrån:
+// https://stackoverflow.com/questions/53773959/adding-event-listener-to-multiple-elements-using-for-loop-and-passing-different
 for (let i = 0; i < checkbox.length; i++) {
   checkbox[i].addEventListener("click", () => {
     seats.push(checkbox[i].id);
   });
 }
 
-//Sparar ner arrayen i en local storage.
+//Sparar ner arrayen i en local storage. Information hämtad härifrån: https://www.geeksforgeeks.org/how-to-store-an-array-in-localstorage/
 function saveSeats() {
   localStorage.selectedSeats = JSON.stringify(seats);
 }
